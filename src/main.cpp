bc462825f8c138a3e8c9fb238bdf3c98e06d5857
Notch-FGJ <mail.fgj.com@gmail.com>
2024-04-08 20:04:04 +0800

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,8 @@
-#include <iostream>
-#include <windows.h>
-
-std::ostream &operator<<(std::ostream &os, WCHAR c)
-{
-    DWORD n;
-    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), &c, 1, &n, NULL);
-    return os;
-}
-
-std::ostream &operator<<(std::ostream &os, const WCHAR *c)
-{
-    DWORD n;
-    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), c, lstrlenW(c), &n, NULL);
-    return os;
-}
+#include "game.h"
 
 int main()
 {
-    std::cout << L'\u2659' << std::endl;
-    std::cout << L"\u2659\u2659\u2659" << std::endl;
+    Game game;
+    game.start();
     return 0;
 }
